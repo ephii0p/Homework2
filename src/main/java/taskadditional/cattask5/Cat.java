ec1013cb0e131f2c52e574c6849e546a0bbddf02
ephii0p <belunkind@gmail.com>
2023-10-31 11:22:30 +0400

    commit

--- src/main/java/taskadditional/cattask5/Cat.java
+++ src/main/java/taskadditional/cattask5/Cat.java
@@ -1,37 +1,30 @@
 package taskadditional.cattask5;
 
 public class Cat {
-    public String name;
-    public int age;
-    public int weight;
+    private String name;
+    private int age;
+    private int weight;
 
     public Cat(String name, int age, int weight) {
         this.name = name;
         this.age = age;
         this.weight = weight;
     }
-
-    public Cat() {
-            }
-        public String getName () {
+            public String getName () {
             return name;
         }
-
         public void setName (String name){
             this.name = name;
         }
-
         public int getAge () {
             return age;
         }
-
         public void setAge ( int age){
             this.age = age;
         }
     public int getWeight() {
         return weight;
     }
-
     public void setWeight(int weight) {
         this.weight = weight;
     }
